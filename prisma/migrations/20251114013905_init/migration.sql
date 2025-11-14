78605fa0c83c1eacca7e559ede4fb6b37637a6df
Chetti666 <a.guiachetti@gmail.com>
2025-11-13 22:39:35 -0300

    migrate init

--- prisma/migrations/20251114013905_init/migration.sql
+++ prisma/migrations/20251114013905_init/migration.sql
@@ -1,9 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
